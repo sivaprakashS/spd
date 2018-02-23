87fbcaab768639a809b1d1896c67aa3dd267e29f
sivaprakashS <31438994+sivaprakashS@users.noreply.github.com>
2018-02-23 16:40:21 +0530

    ih.c

---
+++ lh.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void main()
+{
+int a[100],b,n,i,j;
+printf("enter the number");
+scanf("%d\n",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d\t",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+for(j=i+1;j<n;j++)
+{
+	if(a[i]>a[j])
+	{            
+		b=a[i];
+		a[i]=a[j];
+		a[j]=b;
+    }
+}
+printf("%d%d\n",a[0],a[n-1]);
+}
+
+
+
